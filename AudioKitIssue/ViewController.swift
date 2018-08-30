407e7fa65b9a65ed6edb0bdd8741639c19d5feab
Martin 'Arkardiusz' Mlostek <mlostek@gmail.com>
2018-08-30 09:30:05 +0200

    basic project files

--- AudioKitIssue/ViewController.swift
+++ AudioKitIssue/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var sampler: AKAppleSampler? = nil
+
+    @IBAction func onButtonSetup(_ sender: Any) {
+        do {
+            self.sampler = AKAppleSampler()
+            if let url = Bundle.main.url(forResource: "piano", withExtension: "aupreset") {
+                let presetPath: String = url.path
+                try sampler?.loadPath(presetPath)
+                AudioKit.output = sampler
+            }
+
+        } catch let error as NSError {
+            print(error)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onStop(_ sender: Any) {
+        do {
+            try AudioKit.stop()
+        } catch let error as NSError {
+            print(error)
+        }
     }
 
+    @IBAction func onStart(_ sender: Any) {
+        do {
+            try AudioKit.start()
+        } catch let error as NSError {
+            print(error)
+        }
+    }
 
+    @IBAction func onPlay(_ sender: Any) {
+        do {
+            try sampler?.play(noteNumber: 60, velocity: 100)
+        } catch let error as NSError {
+            print(error)
+        }
+    }
 }
 
